153e7cc67d577c4b650cff178ecb7ae0bc7a704a
Gianluca Giacometti <gianluca.giacometti@unipd.it>
2012-03-11 22:29:59 +0100

    first commit

---
+++ aliases.js
@@ -0,0 +1,241 @@
+/*
+
+ +-----------------------------------------------------------------------+
+ | PostfixAdmin Aliases Plugin for RoundCube                             |
+ | Version: 0.7.1                                                        |
+ | Author: Gianluca Giacometti <php@gianlucagiacometti.it>               |
+ | Copyright (C) 2012 Gianluca Giacometti                                |
+ | License: GNU General Public License                                   |
+ +-----------------------------------------------------------------------+
+
+ code structure based on:
+
+ +-----------------------------------------------------------------------+
+ | SieveRules Module for RoundCube                                       |
+ | by Philip Weir                                                        |
+ | Licensed under the GNU GPL                                            |
+ +-----------------------------------------------------------------------+
+
+*/
+
+rcube_webmail.prototype.aliases_select = function(list) {
+	var id;
+	if (this.aliases_timer) {
+		clearTimeout(rcmail.aliases_timer);
+		}
+	if (id = list.get_single_selection()) {
+		rcmail.aliases_timer = window.setTimeout(function() { rcmail.aliases_load(id, 'plugin.aliases.edit'); }, 200);
+		}
+	}
+
+rcube_webmail.prototype.aliases_keypress = function(list) {
+	if (list.key_pressed == list.DELETE_KEY) {
+		rcmail.command('plugin.aliases.delete');
+		}
+	else if (list.key_pressed == list.BACKSPACE_KEY) {
+		rcmail.command('plugin.aliases.delete');
+		}
+	}
+
+rcube_webmail.prototype.aliases_mouse_up = function(e) {
+	if (rcmail.aliases_list) {
+		if (!rcube_mouse_is_over(e, rcmail.aliases_list.list)) {
+			rcmail.aliases_list.blur();
+			}
+		}
+	}
+
+rcube_webmail.prototype.aliases_load = function(id, action) {
+
+	if (action == 'plugin.aliases.edit' && (!id || id==rcmail.env.iid)) {
+		return false;
+		}
+
+	var add_url = '';
+	var target = window;
+	if (rcmail.env.contentframe && window.frames && window.frames[rcmail.env.contentframe]) {
+		add_url = '&_framed=1';
+		target = window.frames[rcmail.env.contentframe];
+		rcube_find_object(rcmail.env.contentframe).style.visibility = 'inherit';
+		}
+
+	if (action && (id || action == 'plugin.aliases.add')) {
+		rcmail.set_busy(true);
+		target.location.href = rcmail.env.comm_path+'&_action='+action+'&_iid='+id+add_url;
+		}
+
+	return true;
+
+	}
+
+rcube_webmail.prototype.aliases_update_list = function(action, param1, param2, param3, param4) {
+
+	var selection;
+	var rows = rcmail.aliases_list.rows;
+	var rules = Array();
+
+	switch(action) {
+		case 'add-first':
+			rcmail.aliases_list.clear();
+		case 'add':
+			if (rows.length == 1 && rows[0].obj.cells[0].innerHTML == rcmail.gettext('loading','')) {
+				rcmail.aliases_list.remove_row(0);
+				}
+			var newrow = document.createElement('tr');
+			if (param1 == -1) {
+				var cell = document.createElement('td');
+				cell.appendChild(document.createTextNode(param2));
+				newrow.appendChild(cell);
+				}
+			else {
+				newrow.id = param1;
+				var cell = document.createElement('td');
+				cell.appendChild(document.createTextNode(param2));
+				newrow.appendChild(cell);
+				}
+			rcmail.aliases_list.insert_row(newrow);
+			break;
+		case 'update':
+			rcmail.http_request('plugin.aliases.update_list', '', false);
+			break;
+		case 'delete':
+			rcmail.aliases_list.clear_selection();
+		case 'reload':
+			rcmail.aliases_list.clear();
+			var newrow = document.createElement('tr');
+			var cell = document.createElement('td');
+			cell.appendChild(document.createTextNode(rcmail.gettext('loading','')));
+			newrow.appendChild(cell);
+			rcmail.aliases_list.insert_row(newrow);
+			rcmail.http_request('plugin.aliases.update_list', '', false);
+			break;
+		}
+
+	}
+
+rcube_webmail.prototype.aliases_get_index = function(list, value, fallback) {
+	fallback = fallback || 0;
+	for (var i = 0; i < list.length; i++) {
+		if (list[i].value == value) {
+			return i;
+			}
+		}
+	return fallback;
+	}
+
+rcube_webmail.prototype.aliases_load_setup = function() {
+	var add_url = '';
+	var target = window;
+	if (rcmail.env.contentframe && window.frames && window.frames[rcmail.env.contentframe]) {
+		add_url = '&_framed=1';
+		target = window.frames[rcmail.env.contentframe];
+		rcube_find_object(rcmail.env.contentframe).style.visibility = 'inherit';
+		}
+	target.location.href = rcmail.env.comm_path+'&_action=plugin.aliases.setup' + add_url;
+	}
+
+$(document).ready(function() {
+
+	if (window.rcmail) {
+
+		rcmail.addEventListener('init', function(evt) {
+
+			if (rcmail.env.action == 'plugin.aliases.add' || rcmail.env.action == 'plugin.aliases.edit' || rcmail.env.action == 'plugin.aliases.setup' || rcmail.env.action == 'plugin.aliases.advanced') {
+				var tab = $('<span>').attr('id', 'settingstabpluginaliases').addClass('tablink-selected');
+				}
+			else {
+				var tab = $('<span>').attr('id', 'settingstabpluginaliases').addClass('tablink');
+				}
+
+			var button = $('<a>').attr('href', rcmail.env.comm_path+'&_action=plugin.aliases').attr('title', rcmail.gettext('aliasesmanagealiases', 'aliases')).html(rcmail.gettext('aliases','aliases')).appendTo(tab);
+
+			// add button and register command
+			rcmail.add_element(tab, 'tabs');
+
+			if ((rcmail.env.action == 'plugin.aliases' || rcmail.env.action == 'plugin.aliases.advanced') && !rcmail.env.aliaseserror) {
+
+				if (rcmail.gui_objects.aliases_list) {
+					rcmail.aliases_list = new rcube_list_widget(rcmail.gui_objects.aliases_list, {multiselect:false, draggable:true, keyboard:true});
+					rcmail.aliases_list.addEventListener('select', function(o) { rcmail.aliases_select(o); });
+					rcmail.aliases_list.addEventListener('keypress', function(o) { rcmail.aliases_keypress(o); });
+					document.onmouseup = function(e) { return rcmail.aliases_mouse_up(e); };
+					rcmail.aliases_list.init();
+					rcmail.aliases_list.focus();
+
+					if (rcmail.env.iid && rcmail.env.iid < rcmail.aliases_list.rows.length && !rcmail.env.eid) {
+						rcmail.aliases_list.select_row(rcmail.env.iid, false, false);
+						}
+					}
+
+				if (rcmail.env.action == 'plugin.aliases') {
+					rcmail.register_command('plugin.aliases.add', function(id) {
+							if (rcmail.aliases_examples) { rcmail.aliases_examples.clear_selection(); }
+							rcmail.aliases_list.clear_selection();
+							var add_url = '';
+							var target = window;
+							if (rcmail.env.contentframe && window.frames && window.frames[rcmail.env.contentframe]) {
+								add_url = '&_framed=1';
+								target = window.frames[rcmail.env.contentframe];
+								rcube_find_object(rcmail.env.contentframe).style.visibility = 'inherit';
+								}
+							target.location.href = rcmail.env.comm_path+'&_action=plugin.aliases.add' + add_url;
+						}, true);
+					}
+
+				}
+			else if (rcmail.env.action == 'plugin.aliases.setup') {
+				rcmail.register_command('plugin.aliases.import', function(props) {
+					var add_url = '';
+					var target = window;
+					if (rcmail.env.framed)
+						target = window.parent;
+					target.location.href = './?_task=settings&_action=plugin.aliases.import&' + props;
+					}, true);
+				}
+
+			if (rcmail.env.action == 'plugin.aliases.add' || rcmail.env.action == 'plugin.aliases.edit') {
+
+				rcmail.register_command('plugin.aliases.save', function() {
+					var rows;
+					if (rcmail.env.framed) {
+						rows = parent.rcmail.aliases_list.rows;
+						}
+					else {
+						rows = rcmail.aliases_list.rows;
+						}
+					var input_name = rcube_find_object('_name');
+					if (input_name && input_name.value == '') {
+						alert(rcmail.gettext('aliasesnoaliasname','aliases'));
+						input_name.focus();
+						return false;
+						}
+					for (var i = 0; i < rows.length; i++) {
+						if (input_name.value == rows[i].obj.cells[0].innerHTML && i != rcmail.env.iid) {
+							alert(rcmail.gettext('aliasesaliasexists','aliases'));
+							input_name.focus();
+							return false;
+							}
+						}
+					rcmail.gui_objects.editform.submit();
+					}, true);
+
+				rcmail.register_command('plugin.aliases.delete', function(id) {
+					if (confirm(rcmail.gettext('aliasesaliasdeleteconfirm','aliases'))) {
+						var add_url = '';
+						var target = window;
+						if (rcmail.env.contentframe && window.frames && window.frames[rcmail.env.contentframe]) {
+							add_url = '&_framed=1';
+							target = window.frames[rcmail.env.contentframe];
+							rcube_find_object(rcmail.env.contentframe).style.visibility = 'inherit';
+							}
+						target.location.href = rcmail.env.comm_path+'&_action=plugin.aliases.delete&_iid=' + rcmail.env.iid;
+						}
+					}, true);
+
+				}
+
+			});
+
+		}
+
+	});
---
+++ aliases.php
@@ -0,0 +1,364 @@
+<?php
+
+/*
+
+ +-----------------------------------------------------------------------+
+ | PostfixAdmin Aliases Plugin for RoundCube                             |
+ | Version: 0.7.1                                                        |
+ | Author: Gianluca Giacometti <php@gianlucagiacometti.it>               |
+ | Copyright (C) 2012 Gianluca Giacometti                                |
+ | License: GNU General Public License                                   |
+ +-----------------------------------------------------------------------+
+
+ code structure based on:
+
+ +-----------------------------------------------------------------------+
+ | SieveRules Module for RoundCube                                       |
+ | by Philip Weir                                                        |
+ | Licensed under the GNU GPL                                            |
+ +-----------------------------------------------------------------------+
+
+*/
+
+class aliases extends rcube_plugin {
+
+	public $task = 'settings';
+	private $alias;
+	private $action;
+	private $rc;
+
+	function check_driver_error($ret) {
+		switch ($ret) {
+			case PLUGIN_ERROR_CONNECT:
+				$this->rc->output->command('display_message', $this->gettext('aliasesdriverconnecterror'), 'error');
+				$this->init_setup();
+				return FALSE;
+				break;
+			case PLUGIN_ERROR_PROCESS:
+				$this->rc->output->command('display_message', $this->gettext('aliasesdriverprocesserror'), 'error');
+				$this->init_setup();
+				return FALSE;
+				break;
+			case PLUGIN_SUCCESS:
+			default:
+				return TRUE;
+				break;
+			}
+		}
+
+	function init() {
+
+		$rcmail = rcmail::get_instance();
+		$this->rc = &$rcmail;
+		$this->load_config();
+		$this->require_plugin('jqueryui');
+
+		$this->action = $this->rc->action;
+
+		$this->add_texts('localization/', true);
+		$this->rc->output->add_label('aliases');
+
+		$this->include_script('aliases.js');
+
+		$this->register_action('plugin.aliases', array($this, 'init_html'));
+		$this->register_action('plugin.aliases.add', array($this, 'init_html'));
+		$this->register_action('plugin.aliases.edit', array($this, 'init_html'));
+		$this->register_action('plugin.aliases.save', array($this, 'save'));
+		$this->register_action('plugin.aliases.delete', array($this, 'delete'));
+		$this->register_action('plugin.aliases.update_list', array($this, 'gen_js_list'));
+		$this->register_action('plugin.aliases.setup', array($this, 'init_setup'));
+
+		}
+
+	function init_html() {
+
+		$this->_startup();
+
+		$this->api->output->add_handlers(array(
+			'aliaseslist' => array($this, 'gen_list'),
+			'aliasform' => array($this, 'gen_form'),
+			'aliasesframe' => array($this, 'aliases_frame'),
+			));
+
+		$this->api->output->include_script('list.js');
+
+
+		if ($this->action == 'plugin.aliases.add') {
+			$this->api->output->set_pagetitle($this->gettext('aliasesnewalias'));
+			$this->api->output->send('aliases.editalias');
+			}
+
+		elseif ($this->action == 'plugin.aliases.edit') {
+			$this->api->output->set_pagetitle($this->gettext('aliaseseditalias'));
+			$this->api->output->send('aliases.editalias');
+			}
+
+		else {
+			$this->api->output->set_pagetitle($this->gettext('aliases'));
+			$this->api->output->send('aliases.aliases');
+			}
+
+		}
+
+	function init_setup() {
+		$this->_startup();
+		$this->api->output->set_pagetitle($this->gettext('aliases'));
+		$this->api->output->send('aliases.setupaliases');
+		}
+
+	function aliases_frame($attrib) {
+		if (!$attrib['id']) {
+			$attrib['id'] = 'rcmprefsframe';
+			}
+		$attrib['name'] = $attrib['id'];
+		$this->api->output->set_env('contentframe', $attrib['name']);
+		$this->api->output->set_env('blankpage', $attrib['src'] ? $this->api->output->abs_url($attrib['src']) : 'program/blank.gif');
+		return html::iframe($attrib);
+		}
+
+	function gen_list($attrib) {
+
+		$this->api->output->add_gui_object('aliases_list', 'aliases-table');
+
+		$table = new html_table(array('id' => 'aliases-table', 'class' => 'records-table', 'cellspacing' => '0', 'cols' => 1));
+		$table->add_header(null, $this->gettext('aliases'));
+
+		if (sizeof($this->alias) == 0) {
+			$table->add(null, rep_specialchars_output($this->gettext('aliasesnoalias')));
+			$table->add_row();
+			}
+		else foreach($this->alias as $idx => $alias) {
+			$table->set_row_attribs(array('id' => 'rcmrow' . $idx));
+			$table->add(null, Q($alias['name']));
+			}
+
+		return html::tag('div', array('id' => 'aliases-list-filters'), $table->show());
+
+		}
+
+
+	function gen_js_list() {
+
+		$this->_startup();
+
+		if (empty($this->alias)) {
+			$this->api->output->command('aliases_update_list', 'add-first', -1, rep_specialchars_output($this->gettext('aliasesnoalias')));
+			}
+		else foreach($this->alias as $idx => $alias) {
+			$alias_name = $alias['name'];
+			$tmp_output = new rcube_template('settings');
+			$this->api->output->command('aliases_update_list', $idx == 0 ? 'add-first' : 'add', 'rcmrow' . $idx, Q($alias_name));
+			}
+
+		$this->api->output->send();
+
+		}
+
+
+	function gen_form($attrib) {
+
+		$this->include_script('jquery.maskedinput.js');
+		$this->api->output->add_label(
+			'aliases.aliasesaliasdeleteconfirm',
+			'aliases.aliasesaliasexists',
+			'aliases.aliasesnoaliasname'
+			);
+
+		$iid = get_input_value('_iid', RCUBE_INPUT_GPC);
+		if ($iid == '') {
+			$iid = sizeof($this->alias);
+			}
+
+		$cur_alias = $this->alias[$iid];
+		$this->api->output->set_env('iid', $iid);
+		if (isset($this->alias[$iid])) {
+			$this->api->output->add_script("parent.". JS_OBJECT_NAME .".aliases_list.highlight_row(".$iid.");");
+			}
+
+		list($form_start, $form_end) = get_form_tags($attrib, 'plugin.aliases.save');
+
+		$out = $form_start;
+
+		$hidden_iid = new html_hiddenfield(array('name' => '_iid', 'value' => $iid));
+		$out .= $hidden_iid->show();
+
+		$field_id = 'rcmfd_name';
+		$input_name = new html_inputfield(array('name' => '_name', 'id' => $field_id));
+
+		$out .= html::label($field_id, Q($this->gettext('aliasesaliasname')));
+		$out .= "&nbsp;" . $input_name->show($cur_alias['name']);
+
+		$out .= "<br /><br />";
+
+		$out .= $form_end;
+
+		return $out;
+
+		}
+
+	function save() {
+
+		$this->_startup();
+
+		$name = trim(get_input_value('_name', RCUBE_INPUT_POST, true));
+		$iid = trim(get_input_value('_iid', RCUBE_INPUT_POST));
+
+		$driver = $this->home . '/lib/drivers/' . $this->rc->config->get('aliases_driver', 'sql').'.php';
+
+		if (!is_readable($driver)) {
+			raise_error(array('code' => 600, 'type' => 'php', 'file' => __FILE__, 'message' => "aliases plugin: Unable to open driver file $driver"), true, false);
+			return $this->gettext('aliasesinternalerror');
+			}
+
+		require_once($driver);
+
+		if (!function_exists('mail_alias')) {
+			raise_error(array('code' => 600, 'type' => 'php', 'file' => __FILE__, 'message' => "aliases plugin: function mail_alias_read not found in driver $driver"), true, false);
+			return $this->gettext('aliasesinternalerror');
+			}
+
+		$data = array();
+		$data['goto'] = rcmail::get_instance()->user->get_username();
+		$elements = explode("@", trim($data['goto']));
+		$data['address'] = $name . "@" . $elements[1];
+		$data['domain'] = $elements[1];
+
+// check alias existence in domain
+		$ret = mail_alias('allaliases', $data);
+		if (!$this->check_driver_error($ret)) { return FALSE; }
+		foreach ($data['address'] as $alias) {
+			$elements = explode("@", trim($alias['address']));
+			if ($elements[0] == $name) {
+				$this->api->output->command('display_message', $this->gettext('aliasesaliasexistsindomain'), 'error');
+				$this->api->output->add_script("parent.". JS_OBJECT_NAME .".aliases_update_list('update', '0', '". Q($name) ."');");
+				$this->init_setup();
+				return FALSE;
+				}
+			}
+
+		$data['goto'] = $elements[0] . "@" . $elements[1];
+		$elements = explode("@", trim($data['goto']));
+		$data['address'] = $name . "@" . $elements[1];
+		$ret = mail_alias('read', $data);
+		if (!$this->check_driver_error($ret)) { return FALSE; }
+
+// create new alias
+		if (!isset($this->alias[$iid]) && ($name != "") && empty($data)) {
+			$data = array();
+			$data['goto'] = rcmail::get_instance()->user->get_username();
+			$elements = explode("@", trim($data['goto']));
+			$data['address'] = $name . "@" . $elements[1];
+			$data['domain'] = $elements[1];
+			$data['created'] = date('Y-m-d H:i:s');
+			$data['modified'] = date('Y-m-d H:i:s');
+			$data['active'] = 'TRUE';
+			$ret = mail_alias('create', $data);
+			if (!$this->check_driver_error($ret)) { return FALSE; }
+			$this->api->output->command('display_message', $this->gettext('aliasesaliascreated'), 'confirmation');
+			$this->api->output->add_script("parent.". JS_OBJECT_NAME .".aliases_update_list('update', '0', '". Q($name) ."');");
+			$this->init_setup();
+			}
+// update existing alias
+		else {
+			$data = array();
+			$data['goto'] = rcmail::get_instance()->user->get_username();
+			$elements = explode("@", trim($data['goto']));
+			$data['address'] = $this->alias[$iid]['name'] . "@" . $elements[1];
+			$data['newalias'] = $name . "@" . $elements[1];
+			$ret = mail_alias('update', $data);
+			if (!$this->check_driver_error($ret)) { return FALSE; }
+			$this->api->output->command('display_message', $this->gettext('aliasesaliasupdated'), 'confirmation');
+			$this->api->output->add_script("parent.". JS_OBJECT_NAME .".aliases_update_list('update', '0', '". Q($name) ."');");
+			$this->init_setup();
+			}
+
+		$this->alias = array();
+		$this->_startup();
+
+		rcmail_overwrite_action('plugin.aliases.edit');
+		$this->action = 'plugin.aliases.edit';
+		$this->init_html();
+
+		}
+
+	function delete() {
+
+		$this->_startup();
+
+		$driver = $this->home . '/lib/drivers/' . $this->rc->config->get('aliases_driver', 'sql').'.php';
+
+		if (!is_readable($driver)) {
+			raise_error(array('code' => 600, 'type' => 'php', 'file' => __FILE__, 'message' => "aliases plugin: Unable to open driver file $driver"), true, false);
+			return $this->gettext('aliasesinternalerror');
+			}
+
+		require_once($driver);
+
+		if (!function_exists('mail_alias')) {
+			raise_error(array('code' => 600, 'type' => 'php', 'file' => __FILE__, 'message' => "aliases plugin: function mail_alias_read not found in driver $driver"), true, false);
+			return $this->gettext('aliasesinternalerror');
+			}
+
+		$data = array();
+		$data['goto'] = rcmail::get_instance()->user->get_username();
+		$elements = explode("@", trim($data['goto']));
+		$ids = get_input_value('_iid', RCUBE_INPUT_GET);
+		$data['address'] = $this->alias[$ids]['name'] . "@" . $elements[1];
+
+		$ret = mail_alias('delete', $data);
+		if (!$this->check_driver_error($ret)) { return FALSE; }
+
+		$this->alias = array();
+		$this->_startup();
+
+		$this->api->output->command('display_message', $this->gettext('aliasesaliasdeleted'), 'confirmation');
+		$this->api->output->add_script("parent.". JS_OBJECT_NAME .".aliases_update_list('delete', ". $ids .");");
+
+		if (isset($_GET['_framed']) || isset($_POST['_framed'])) {
+			$this->api->output->add_script("parent.". JS_OBJECT_NAME .".show_contentframe(false);");
+			}
+		else {
+			rcmail_overwrite_action('plugin.aliases.edit');
+			$this->action = 'plugin.aliases.edit';
+			$this->init_html();
+			}
+
+		}
+
+	private function _startup() {
+
+		$driver = $this->home . '/lib/drivers/' . $this->rc->config->get('aliases_driver', 'sql').'.php';
+
+		if (!is_readable($driver)) {
+			raise_error(array('code' => 600, 'type' => 'php', 'file' => __FILE__, 'message' => "aliases plugin: Unable to open driver file $driver"), true, false);
+			return $this->gettext('aliasesinternalerror');
+			}
+
+		require_once($driver);
+
+		if (!function_exists('mail_alias')) {
+			raise_error(array('code' => 600, 'type' => 'php', 'file' => __FILE__, 'message' => "aliases plugin: function mail_alias_read not found in driver $driver"), true, false);
+			return $this->gettext('aliasesinternalerror');
+			}
+
+		$data = array();
+		$data['goto'] = rcmail::get_instance()->user->get_username();
+
+		$ret = mail_alias('aliases', $data);
+		if (!$this->check_driver_error($ret)) { return FALSE; }
+
+		foreach ($data['address'] as $alias) {
+			$elements = explode("@", trim($alias['address']));
+			if ($elements[0] != "") {
+				$this->alias[] = array("name" => $elements[0]);
+				}
+			}
+		sort($this->alias);
+
+		return TRUE;
+
+		}
+
+	}
+
+?>
